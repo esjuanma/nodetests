b7c76a5a82bab30657e37ee1bb55b45c7f9a4d74
Juan Elfers <esjuanma@hotmail.com>
2018-10-23 17:19:36 -0300

    testings

--- 1.js
+++ 1.js
@@ -1,14 +1,37 @@
-const myModule = require('./myModule');
+// Native
 const http = require('http');
 const fs = require('fs');
 
-http.createServer((req, res) => {
-	fs.readFile('demofile1.html', 'utf8', (err, html) => {
-		const content = `It's ${myModule.myDateTime()}`;
+// Third party
+var url = require('url');
 
-		res.writeHead(200, { 'Content-Type': 'text/html' });
+// Custom
+const myModule = require('./myModule');
+// const helpers = require('./helpers');
+
+http.createServer((req, res) => {
+	console.log('req.url', req.url);
+	const content = `It's ${myModule.myDateTime()}`;
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	
+	fs.readFile('demofile1.html', 'utf8', (err, html) => {
 		res.write(html.replace('{{content}}', content));
 		res.end();
+
+		const query = url.parse(req.url, true);
+		console.log('query', query);
+		
+		const r = {};
+		for(const key in req) {
+			if(typeof req[key] !== 'object' || req[key] instanceof Array) {
+				r[key] = req[key];
+			}
+		}
+
+		fs.writeFile('json.txt', JSON.stringify(r), function (err) {
+			if (err) throw err;
+			console.log('- request saved');
+		});
 	});
 }).listen(8080);
 
